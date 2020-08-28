4a17a669c946e20008644518c3b682e4f29fbedb
Maciek Sitkowski <msitkowski94@gmail.com>
2020-08-28 10:12:56 +0200

    Updates

---
+++ precache-manifest.57f576234d50ef534500bce153cdf243.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf75ea1634133fb2b2908e76424a6f1e",
+    "url": "/simple-markdown-previewer/index.html"
+  },
+  {
+    "revision": "884dd3ffc59f745e1ef4",
+    "url": "/simple-markdown-previewer/static/css/main.ea4d098e.chunk.css"
+  },
+  {
+    "revision": "c7e0d79c37933066c0ad",
+    "url": "/simple-markdown-previewer/static/js/2.f390659a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-markdown-previewer/static/js/2.f390659a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "884dd3ffc59f745e1ef4",
+    "url": "/simple-markdown-previewer/static/js/main.92831b86.chunk.js"
+  },
+  {
+    "revision": "611088b896b961463511",
+    "url": "/simple-markdown-previewer/static/js/runtime-main.d6c32e5b.js"
+  }
+]);
